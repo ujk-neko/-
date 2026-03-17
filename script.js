208870b122992a0975a5fdbef7cedc3b1cb3e499
ujk-neko <h10624048@stu.tochigi-edu.ed.jp>
2026-03-18 00:56:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // 画像のリスト（あなたのファイル名に合わせて書き換えてください）
-const images = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg"];
+const images = ["01.jpg", "02.jpg", "03.jpg"];
 let currentIndex = 0;
 
 const imgElement = document.getElementById("mangaPage");
